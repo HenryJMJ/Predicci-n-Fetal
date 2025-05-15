21907aeb5ba020e0fa1de341feecb588bf5c8cab
HenryJMJ <henrymestra16@gmail.com>
2025-05-14 23:52:30 -0500

    Actualizar settings.py para despliegue

--- ProyectoFinal/settings.py
+++ ProyectoFinal/settings.py
@@ -11,21 +11,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-ax!4glh(e@c6kjz&x4yx96q*6_c&jhi!+eg4l-u^km7bi5=#=z')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'  # Por defecto True si no está definido
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ax!4glh(e@c6kjz&x4yx96q*6_c&jhi!+eg4l-u^km7bi5=#=z'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,predicci-n-fetal.onrender.com').split(',')
 
 
 # Application definition
